8552e92cfe9ca42afc27bbde5b554cec7e5712dc
burak ataseven <buraqataseven@gmail.com>
2019-01-31 14:31:38 +0300

    first

---
+++ precache-manifest.1c7eb7ddca9d8308c8ab4df89a93312f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "c585c9ba930bcbad212d",
+    "url": "/pages/home.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "70f1b5bb3d0977037c24",
+    "url": "/pages/projects.js"
+  },
+  {
+    "revision": "94aef42deb58df9eedfd",
+    "url": "/pages/404.js"
+  },
+  {
+    "revision": "1ef0e46846cd7adec965",
+    "url": "/pages/../vendor.js"
+  },
+  {
+    "revision": "857f8a7e2a501a566e70db8cda9e2656",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70f1b5bb3d0977037c24",
+    "url": "/css/projects.css"
+  },
+  {
+    "revision": "c585c9ba930bcbad212d",
+    "url": "/css/home.css"
+  },
+  {
+    "revision": "94aef42deb58df9eedfd",
+    "url": "/css/404.css"
+  },
+  {
+    "revision": "440a615708a0812dce21",
+    "url": "/bundle.js"
+  },
+  {
+    "revision": "440a615708a0812dce21",
+    "url": "/bundle.css"
+  },
+  {
+    "revision": "cbf8ff8f5819a370d4f6f2ed67498845",
+    "url": "/404.html"
+  }
+];
